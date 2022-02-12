960fadb6720d986c18caa27cba36ba38d058b07e
EVGENY_SE <74773570+evgeny2020se@users.noreply.github.com>
2022-02-12 08:49:00 +0300

    Add files via upload

---
+++ RegistrAuth/RegistrAuth/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+
+namespace RegistrAuth
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mananger.frame = Mainframe;
+            Mainframe.Navigate(new Pages.AuthPage());
+        }
+    }
+}
---
+++ RegistrAuth/RegistrAuth/Mananger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RegistrAuth
+{
+    internal class Mananger
+    {
+        public static Frame frame { get; set; }
+    }
+}
